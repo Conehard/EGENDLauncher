4cb49834b6b9240c525222549c454a39c764e70b
André Augusto Assunção <theconehard@gmail.com>
2019-03-11 01:33:36 -0300

    Primeiro Commit

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(742, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(910, 433);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://egend.com.br", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 475);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(701, 30);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 508);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Arquivo X de N";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EGEND LAUNCHER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using System.ComponentModel;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        static string urlVersao = "http://egend.com.br/top/att/vrsn.egz";
+        string downloadDestination = Path.GetTempFileName();
+        string version = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void buscarArquivo()
+        {
+            WebClient webClient = new WebClient();
+
+            label1.Text = "Buscando atualização...";
+
+            string versaoRemota = webClient.DownloadString(urlVersao).Trim();
+            string[] partesArquivo = (new Regex(@"\s+")).Split(versaoRemota);
+            string urlRemota = partesArquivo[1];
+            string hashRemoto = partesArquivo[2];
+            version = partesArquivo[0];
+
+            Version versaoLocal = new Version(File.ReadAllText("vrsn.egz").Trim());
+            Version versaoRemotaArquivo = new Version(partesArquivo[0]);
+
+            if (versaoRemotaArquivo > versaoLocal)
+            {
+                label1.Text = "Existe uma nova versão disponivel.";
+                Atualizar(urlRemota, hashRemoto);
+            }
+            else
+            {
+                label1.Text = "Nenhuma atualização disponível.";
+            }
+        }
+
+        public void Atualizar(string urlRemota, string hashRemoto)
+        {
+            label1.Text = "Iniciando atualização";
+
+            label1.Text = "Atualizando...";
+
+            WebClient downloadifier = new WebClient();
+            downloadifier.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            downloadifier.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            downloadifier.DownloadFileAsync(new Uri(urlRemota), downloadDestination);
+
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            string diretorioExtracao = System.Environment.CurrentDirectory + @"./updated";
+            string final = System.Environment.CurrentDirectory;
+            if (Directory.Exists(diretorioExtracao))
+            {
+                Directory.Delete(diretorioExtracao, true);
+            }
+            using (ZipArchive archive = ZipFile.Open(downloadDestination, ZipArchiveMode.Update))
+            {
+                archive.ExtractToDirectory(diretorioExtracao);
+            }
+            CopiarArquivos(diretorioExtracao, final);
+            label1.Text = "Finalizando...";
+            label1.Text = "Limpando arquivos desnecessários - ";
+            Directory.Delete(diretorioExtracao, true);
+            File.WriteAllText("vrsn.egz", version);
+
+            label1.Text = "Atualização finalizada.";
+        }
+
+
+        static string GetSHA1HashFromFile(string fileName)
+        {
+            FileStream file = new FileStream(fileName, FileMode.Open);
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] byteHash = sha1.ComputeHash(file);
+            file.Close();
+
+            StringBuilder hashString = new StringBuilder();
+            for (int i = 0; i < byteHash.Length; i++)
+                hashString.Append(byteHash[i].ToString("x2"));
+            return hashString.ToString();
+        }
+
+        public void CopiarArquivos(string pastaBaixados, string pastaFinal)
+        {
+            if (!Directory.Exists(pastaFinal))
+                Directory.CreateDirectory(pastaFinal);
+
+            string[] arquivos = Directory.GetFiles(pastaBaixados);
+            foreach (string arquivo in arquivos)
+            {
+                string nome = Path.GetFileName(arquivo);
+                string destino = Path.Combine(pastaFinal, nome);
+                File.Copy(arquivo, destino, true);
+            }
+
+            string[] pastas = Directory.GetDirectories(pastaBaixados);
+            foreach (string pasta in pastas)
+            {
+                string nome = Path.GetFileName(pasta);
+                string destino = Path.Combine(pastaFinal, nome);
+                CopiarArquivos(pasta, destino);
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename = Path.Combine(Environment.CurrentDirectory, @"system\Game.exe");
+            var proc = System.Diagnostics.Process.Start(filename, "startgame");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            buscarArquivo();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
